a3408256a6b4f3f11af9665231149785ba0e92d9
Arthur Rodrigues <araruna@gmail.com>
2013-08-04 01:42:50 -0300

    [*] Finished insert operation on BinomialHeap

--- BinomialHeap/BinomialHeap.cpp
+++ BinomialHeap/BinomialHeap.cpp
@@ -7,12 +7,108 @@
 
 #include "BinomialHeap.hpp"
 
-BinomialHeap::BinomialHeap() {
-	// TODO Auto-generated constructor stub
+#ifdef __GNUC__
+#ifndef _LIKHOOD__M
+#define _LIKHOOD__M
+#define likely(x)	__builtin_expect((x),1)
+#define unlikely(x)	__builtin_expect((x),0)
+#endif
+#endif
 
+MemoryPool BinomialHeap::Node::pool(sizeof(BinomialHeap::Node), 30000u);
+
+void*
+BinomialHeap::Node::operator new(size_t size)
+throw (std::length_error) {
+	if(unlikely(size != sizeof(Node)))
+		throw std::length_error("Tentativa de alocacao de mais de um elemento.");
+
+	return pool.take();
+}
+
+void
+BinomialHeap::Node::operator delete(void* ptr) {
+	pool.yield(ptr);
+}
+
+
+BinomialHeap::BinomialHeap(Comparator &comp)
+ : comp(comp) {
+
+	head = lead = NULL;
+	numElems = 0;
 }
 
 BinomialHeap::~BinomialHeap() {
-	// TODO Auto-generated destructor stub
+	// TODO Percorrer toda a arvore apagando os nos
 }
 
+void
+BinomialHeap::insert(T* elem) {
+	insert(elem, static_cast<double>(reinterpret_cast<unsigned long>(elem)));
+}
+
+void
+BinomialHeap::insert(T* elem, double key) {
+	Node *i, *j, *p;
+
+	i = p = head;
+	j = new Node;
+
+	j->size = 1;
+	j->next = j->children = j->last = NULL;
+	j->element = elem;
+	j->key = key;
+
+	if(unlikely(head == NULL)) {
+		head = lead = j;
+	} else {
+		while(i != NULL && i->size < j->size) {
+			p = i;
+			i = i->next;
+		}
+
+		if(i == NULL || i->size > j->size) {
+			j->next = i;
+			p->next = j;
+		} else while(i != NULL && i->size == j->size) {
+			if(comp(i->key, j->key)) {
+				p->next = j;
+				j->next = i->next;
+				i->next = NULL;
+
+				if(j->last == NULL)
+					j->children = j->last = i;
+				else {
+					j->last->next = i;
+					j->last = i;
+				}
+
+				// Dobramos o numero de elementos na arvore *j*
+				j->size <<= 1;
+
+				// *j* eh sempre a nova arvore
+				i = j->next;
+			} else {
+				if(i->last == NULL)
+					i->children = i->last = i;
+				else {
+					i->last->next = j;
+					i->last = j;
+				}
+
+				// Dobramos o numero de elementos na arvore *i*
+				i->size <<= 1;
+
+				// *j* eh sempre a nova arvore
+				j = i;
+				i = i->next;
+			}
+		}
+
+		if(i == NULL)
+			lead = j;
+	}
+
+	++numElems;
+}
--- BinomialHeap/BinomialHeap.hpp
+++ BinomialHeap/BinomialHeap.hpp
@@ -8,10 +8,64 @@
 #ifndef BINOMIALHEAP_HPP_
 #define BINOMIALHEAP_HPP_
 
+#include <new>
+#include <stdexcept>
+#include "../MemoryManager/MemoryPool.hpp"
+
+class T;
+
 class BinomialHeap {
 public:
-	BinomialHeap();
+
+	class Comparator {
+	public:
+		virtual bool operator()(double key1, double key2) {
+			return key1 < key2;
+		}
+	};
+
+	 BinomialHeap(Comparator &comp = ltdef);
 	~BinomialHeap();
+
+	void insert(T* elem);
+	void insert(T* elem, double key);
+
+	T*     peekFirstElem();
+	double peekFirstKey();
+
+	T* remove();
+
+	unsigned long getSize();
+
+private:
+
+	class Node {
+	private:
+		static MemoryPool pool;
+
+		T* element;
+		double key;
+		unsigned long size;
+
+		// *next* aponta para o proximo elemento irmao deste no
+		// *children* aponta para o primeiro no na lista de descendentes deste no
+		// *last* aponta o ultimo filho na lista de descendentes deste no (para melhorar eficiencia de tempo e memoria na mesclagem de arvores)
+		Node *next, *children, *last;
+
+	public:
+		friend class BinomialHeap;
+
+		static void* operator new(size_t size) throw (std::length_error);
+		static void  operator delete(void* ptr);
+	};
+
+	static Comparator ltdef;
+
+	Comparator comp;
+	unsigned long numElems;
+
+	Node *head, *lead;
+
 };
 
 #endif /* BINOMIALHEAP_HPP_ */
